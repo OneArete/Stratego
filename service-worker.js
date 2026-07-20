c8170b7a1b61e02115490f4d52ad300fa3b38c07
possn <pedrossnunes@gmail.com>
2026-07-20 21:33:58 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,13 @@
-const CACHE_NAME = 'strategos-shell-v0.17.1';
+const CACHE_NAME = 'strategos-shell-v0.18.0-phase-1';
 const CORE_ASSETS = [
   './',
   './index.html',
-  './styles.css?v=0171',
+  './styles.css?v=0180p1',
   './manifest.webmanifest',
   './icons/delta-180.png',
   './icons/delta-192.png',
   './icons/delta-512.png',
-  './src/app.js?v=0171'
+  './src/app.js?v=0180p1'
 ];
 
 self.addEventListener('install', event => {
