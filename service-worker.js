44da8287b0ea8b762a969482aa99acd5e069f4c2
possn <pedrossnunes@gmail.com>
2026-07-24 22:22:57 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'strategos-shell-v0.47.5-checkin-choice-interaction-repair';
+const CACHE_NAME = 'strategos-shell-v0.47.6-checkin-interaction-and-symmetry-repair';
 const STATIC_ASSETS = [
   './manifest.webmanifest',
   './icons/delta-180.png',
